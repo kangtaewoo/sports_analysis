dfae634b5a9bf86b6a645a10c4b12c51a8afa578
김민순 <butga@naver.com>
2019-05-22 22:43:09 +0900

    tracking

--- source/tracking.py
+++ source/tracking.py
@@ -24,6 +24,29 @@ while True:
         
     frame = imutils.resize(frame, width=600)
 
+    fgmask = fgbg.apply(frame)
+
+    # 노이즈 제거
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    fgmask = cv2.dilate(fgmask, kernel, iterations=1)
+
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(fgmask)
+    for index, centroid in enumerate(centroids):
+        if stats[index][0] == 0 and stats[index][1] == 0:
+            continue
+        if np.any(np.isnan(centroid)):
+            continue
+
+
+        x, y, width, height, area = stats[index]
+        centerX, centerY = int(centroid[0]), int(centroid[1])
+
+        # if area > 100:
+        #     cv2.circle(frame, (centerX, centerY), 1, (0, 0, 255), 2)
+        #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
+
+
+
     if initBB is not None:
         (success, box) = tracker.update(frame)
 
